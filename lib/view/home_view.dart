706a541f45ce15381436aaf381ee3555f1bfd5fa
Lucas Barradas <lucasbh04@gmail.com>
2025-03-26 16:16:47 -0300

    atualizando SideBar

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -8,7 +9,7 @@ class HomeView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Geral'),
-        backgroundColor: const Color.fromARGB(255, 91, 153, 186),
+        backgroundColor: const Color.fromARGB(255, 74, 177, 233),
         ),
       drawer: const NavigationDrawer()
     );
@@ -20,6 +21,7 @@ class NavigationDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Drawer(
+    backgroundColor: const Color.fromARGB(255, 74, 177, 233),
     child: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -37,26 +39,28 @@ class NavigationDrawer extends StatelessWidget {
     ),
   );
 
-  Widget buildMenuItens(BuildContext context) => Column(
+  Widget buildMenuItens(BuildContext context) => Wrap(
+    runSpacing: 15,
     children: [
       ListTile(
-        leading: const Icon(Icons.home_outlined),
-        title: const Text('Home'),
+        leading: Icon(Icons.home_outlined, size: 30,),
+        title: Text(
+          'Home', style: TextStyle(fontSize: 22),),
         onTap: () {},
       ),
       ListTile(
-        leading: const Icon(Icons.check_box_outlined),
-        title: const Text('Concluidos'),
+        leading: const Icon(Icons.check_box_outlined, size: 30,),
+        title: const Text('Concluidos', style: TextStyle(fontSize: 22),),
         onTap: () {},
       ),
       ListTile(
-        leading: const Icon(Icons.delete_outline),
-        title: const Text('Lixeira'),
+        leading: const Icon(Icons.delete_outline, size: 30,),
+        title: const Text('Lixeira', style: TextStyle(fontSize: 22),),
         onTap: () {},
       ),
       ListTile(
-        leading: const Icon(Icons.archive_outlined),
-        title: const Text('Arquivados'),
+        leading: const Icon(Icons.archive_outlined, size: 30,),
+        title: const Text('Arquivados', style: TextStyle(fontSize: 22),),
         onTap: () {},
       )
     ],
