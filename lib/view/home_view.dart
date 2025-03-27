bf50393bbb336aa5c6ebc8d5979b00e987e28b1f
Lucas Barradas <lucasbh04@gmail.com>
2025-03-27 14:45:32 -0300

    Criando grupos e notas no home

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -6,6 +6,11 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Geral'),
+        backgroundColor: const Color.fromARGB(255, 74, 177, 233),
+        ),
+      drawer: const SideBar(),
       body: Container(
         width: double.infinity,
         height: double.infinity,
@@ -15,24 +20,140 @@ class HomeView extends StatelessWidget {
             fit: BoxFit.cover,
           ),
         ),
-        child: SizedBox(
-          width: MediaQuery.sizeOf(context).width,
-          height: MediaQuery.sizeOf(context).height,
-          child: IconButton(
-            iconSize: 90,
-            onPressed: () {
-              Navigator.of(context).pushNamed('concluidos');
-            },
-            icon: ImageIcon(AssetImage("icons/circle-plus.png")),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: ListView(
+            children: [
+              GroupCard(                        //Adicionando os grupos
+                title: 'Trabalho', 
+                color: Colors.grey[300]!, 
+                onTap: () {
+                  Navigator.of(context).pushNamed('');
+                }
+              ),
+
+              GroupCard(
+                title: 'Pessoal', 
+                color: Colors.purple[400]!, 
+                onTap: () {
+                  Navigator.of(context).pushNamed('');
+                }
+              ),
+
+              GroupCard(
+                title: 'Estudos', 
+                color: Colors.green[400]!, 
+                onTap: () {
+                  Navigator.of(context).pushNamed('');
+                }
+              ),
+            ],
           ),
         ),
       ),
-      appBar: AppBar(
-        title: Text('Geral'),
-        backgroundColor: const Color.fromARGB(255, 74, 177, 233),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.black54,
+        child: PopupMenuButton<String>(
+          onSelected: (value) {
+            if (value == 'novo_grupo') {
+              // Lógica para novo grupo
+            } else if (value == 'nova_nota') {
+              // Lógica para nova nota
+            }
+          },
+          itemBuilder: (context) => [
+            const PopupMenuItem(
+              value: 'novo_grupo',
+              child: Text('Novo Grupo'),
+            ),
+            const PopupMenuItem(
+              value: 'nova_nota',
+              child: Text('Nova Nota'),
+            ),
+          ],
+          child: const Icon(Icons.add, size: 50, color: Colors.black,),
         ),
-      drawer: const SideBar()
-      
+      ),
+    );
+  }
+}
+
+
+class GroupCard extends StatelessWidget {
+  final String title;
+  final Color color;
+  final VoidCallback onTap;
+
+  const GroupCard({
+    super.key,
+    required this.title,
+    required this.color,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Color headerColor = darkenColor(color, 0.7);
+
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          //Nome Grupo
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 6),
+            width: 160,
+            decoration: BoxDecoration(
+              color: headerColor,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+              ),
+            ),
+            child: Text(
+              title,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+          //Corpo Grupo
+          Container(
+            width: 160,
+            height: 150,
+            decoration: BoxDecoration(
+              color: color,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            padding: const EdgeInsets.all(8),
+            child: Stack(
+              children: [
+                // Notas Ilustração
+                Positioned(top: 10, left: 10, child: _buildNote(Colors.blue)),
+                Positioned(top: 10, right: 10, child: _buildNote(Colors.green)),
+                Positioned(bottom: 10, left: 10, child: _buildNote(Colors.orange)),
+                Positioned(bottom: 10, right: 10, child: _buildNote(Colors.red)),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  //Criar Notas Ilustração
+  Widget _buildNote(Color color) {
+    return Container(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(5),
+      ),
     );
   }
 }
@@ -54,13 +175,13 @@ class SideBar extends StatelessWidget {   //Criando Drawer - SideBar
     ),
   );
 
-  Widget buildHeader(BuildContext context) => Container(
+  Widget buildHeader(BuildContext context) => Container(    //Header da SideBar
       padding: EdgeInsets.only(
       top: MediaQuery.of(context).padding.top
     ),
   );
 
-  Widget buildMenuItens(BuildContext context) => Wrap(
+  Widget buildMenuItens(BuildContext context) => Wrap(    //Itens da SideBar
     runSpacing: 15,
     children: [
       ListTile(
@@ -104,4 +225,11 @@ class SideBar extends StatelessWidget {   //Criando Drawer - SideBar
       )
     ],
   );
+}
+
+
+Color darkenColor(Color color, double fator) {  //Escurecer a cor para o topo do grupo
+  HSVColor hsv = HSVColor.fromColor(color);
+  HSVColor novaCor = hsv.withValue((hsv.value * fator).clamp(0.0, 1.0));
+  return novaCor.toColor();
 }
