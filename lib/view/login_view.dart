f8b30983c2a2dac7e3a7f93da4c5fbb0fc2fd274
Lucas Barradas <lucasbh04@gmail.com>
2025-04-03 16:15:37 -0300

    Adicionando logotipo a tela de login

--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -24,53 +24,76 @@ class _LoginViewState extends State<LoginView> {
         padding: const EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextFormField(
-                controller: controller.txtLoginEmail,
-                decoration: InputDecoration(labelText: 'Email'),
-                keyboardType: TextInputType.emailAddress,
-              ),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: Container(
+                    width: 150,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/logo_img.jpg'),
+                        fit: BoxFit.contain,
+                      ),
+                      borderRadius: BorderRadius.circular(25),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.45), 
+                          blurRadius: 15, 
+                          offset: Offset(0, 5),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                TextFormField(
+                  controller: controller.txtLoginEmail,
+                  decoration: InputDecoration(labelText: 'Email'),
+                  keyboardType: TextInputType.emailAddress,
+                ),
 
-              TextFormField(
-                controller: controller.txtLoginSenha,
-                decoration: InputDecoration(labelText: 'Senha'),
-                obscureText: true,
-              ),
+                TextFormField(
+                  controller: controller.txtLoginSenha,
+                  decoration: InputDecoration(labelText: 'Senha'),
+                  obscureText: true,
+                ),
 
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  if (controller.validateField()) {
-                    if (controller.validarLogin(controller.txtLoginEmail.text, controller.txtLoginSenha.text)){
-                      Navigator.pushNamed(context, 'home');
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    if (controller.validateField()) {
+                      if (controller.validarLogin(controller.txtLoginEmail.text, controller.txtLoginSenha.text)){
+                        Navigator.pushNamed(context, 'home');
+                      }
+                      else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Email ou senha incorretos')),
+                        );
+                      }
+                    } else {
+                      controller.showAlertDialog(context);
                     }
-                    else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text('Email ou senha incorretos')),
-                      );
-                    }
-                  } else {
-                    controller.showAlertDialog(context);
-                  }
-                },
-                child: Text('Entrar'),
-              ),
-              
-              TextButton(
-                onPressed: () => Navigator.pushNamed(context, 'esqueci_senha'),
-                child: Text('Esqueci a Senha'),
-              ),
+                  },
+                  child: Text('Entrar'),
+                ),
+                
+                TextButton(
+                  onPressed: () => Navigator.pushNamed(context, 'esqueci_senha'),
+                  child: Text('Esqueci a Senha'),
+                ),
 
-              TextButton(
-                onPressed: () => Navigator.pushNamed(context, 'cadastro'),
-                child: Text('Criar Conta'),
-              ),
-            ],
+                TextButton(
+                  onPressed: () => Navigator.pushNamed(context, 'cadastro'),
+                  child: Text('Criar Conta'),
+                ),
+              ],
+            ),
           ),
         ),
-      ),
+      )
     );
   }
 }
